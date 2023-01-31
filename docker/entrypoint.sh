278e66cb2df1487765c1a041a6167652565e5a93
sljeff <kindjeff.com@gmail.com>
2023-01-31 19:02:12 +0800

    fix: validator entrypoint mkdir

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir -p /opt/keystore/
 cat /vault/secrets/keystore | jq -c -r .data.keystoreValidator01 > /opt/keystore/keystoreValidator01
 cat /vault/secrets/keystore | jq -c -r .data.keystoreValidator02 > /opt/keystore/keystoreValidator02
 exec bridge --config $CONFIG_PATH --verbosity $VERBOSITY
